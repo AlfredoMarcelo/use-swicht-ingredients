5e55c29249aca5442f3fae8518e9c3c975b3d1a7
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-03-22 18:27:59 -0300

    code terminado

---
+++ src/useswitch/UsoSwicth.java
@@ -0,0 +1,24 @@
+package useswitch;
+
+public class UsoSwicth {
+	public static void main(String[] args) {
+		System.out.println("Escribe que quieres cocinar y obten la receta: " + "\n" + "queque" + "\n" + "pan" + "\n" + "pizza" + "\n");
+		
+		String comida = args[0];
+		
+		switch (comida) {
+		case "queque":
+			System.out.println("Receta para hacer queque: " + "\n" + "-Harina" + "\n" + "-Azúcar" + "-Mantequilla" + "\n" + "-Polvo para hornear" + "\n" + "-Escencia de vainilla" + "\n" + "-Huevos" + "\n" + "-Leche");
+			break;
+		case "pan":
+			System.out.println("Receta para hacer pan: " + "\n" + "-Harina" + "\n" + "-Sal" + "\n" + "-Manteca" + "\n" + "-Levadura" + "\n" + "-Agua" + "\n" + "-Leche");
+			break;
+		case "pizza":
+			System.out.println("Receta para hacer pizza: " + "\n" + "-Harina" + "\n" + "-Sal" + "\n" + "-Levadura" + "\n" + "-Queso" + "\n" + "-Aceite" + "\n" + "-Salsa de tomate" + "\n" + "-Aceituna" + "\n" + "-Jamon");
+			break;
+		default:
+			System.out.println("¡¡Debes seleccionar una de las tres comidas en la lista, por favor :| !!");
+			break;
+		}
+	}
+}
